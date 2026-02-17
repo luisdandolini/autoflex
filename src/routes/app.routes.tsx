d5491c0a53ec9eb440a877bd7d2bccf72e36ce38
Luis Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2026-02-16 22:29:52 -0300

    feat: create routes for association and production

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -3,6 +3,8 @@ import { DefaultLayout } from "../components/templates/DefaultLayout";
 import Home from "../pages/Home";
 import ProductsTemplate from "../components/templates/Product";
 import RawMaterialsTemplate from "../components/templates/RawMaterial";
+import ProductionTemplate from "../components/templates/Production";
+import AssociationsTemplate from "../components/templates/Associations";
 
 export const AppRoutes = () => {
   return (
@@ -11,6 +13,8 @@ export const AppRoutes = () => {
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<ProductsTemplate />} />
         <Route path="/raw-materials" element={<RawMaterialsTemplate />} />
+        <Route path="/production" element={<ProductionTemplate />} />
+        <Route path="/associations" element={<AssociationsTemplate />} />
       </Route>
     </Routes>
   );
