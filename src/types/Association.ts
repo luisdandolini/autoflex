b4a32d0dd3984f6fc5a7262146d12942de0dae21
Luis Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2026-02-16 22:29:37 -0300

    feat: create types association and production

---
+++ src/types/Association.ts
@@ -0,0 +1,8 @@
+export interface Association {
+  id: string;
+  quantity_needed: number;
+  raw_material_id: string;
+  raw_material_code: string;
+  raw_material_name: string;
+  quantity_stock: number;
+}
